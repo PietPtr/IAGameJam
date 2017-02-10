0125fead4635ddfc7bfa41bf2dc6eed7943f2662
Pieter Staal <pieterstaal@outlook.com>
2017-02-10 17:45:36 +0100

    Initial

---
+++ include/Game.h
@@ -0,0 +1,39 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Audio.h"
+
+using namespace sf;
+
+class Game
+{
+    public:
+        Game(RenderWindow* window);
+        void initialize();
+        void update();
+        void draw();
+        bool isWindowOpen();
+
+        void loadAudio(std::vector<std::string> fileNames);
+        void loadTextures(std::vector<std::string> fileNames);
+
+        int randint(int low, int high, int seed);
+        int randint(int low, int high);
+    protected:
+    private:
+        RenderWindow* window;
+
+        Time dt;
+        Time totalTime;
+        Clock clock;
+        int frame = 0;
+        bool focus = false;
+
+        int windowWidth = 1280;
+        int windowHeight = 720;
+
+        std::vector<std::string> audioFileNames { };
+        std::vector<std::string> textureFileNames { "test.png" };
+
+        std::vector<Audio*> sfx;
+        std::vector<Texture> textures;
+};
---
+++ src/Game.cpp
@@ -0,0 +1,90 @@
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+using namespace sf;
+
+Game::Game(RenderWindow* _window)
+{
+    window = _window;
+}
+
+void Game::initialize()
+{
+    loadAudio(audioFileNames);
+    loadTextures(textureFileNames);
+}
+
+void Game::update()
+{
+    Event event;
+    while (window->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window->close();
+        if (event.type == Event::KeyPressed)
+        {
+            if (event.key.code == Keyboard::Escape)
+            {
+                window->close();
+            }
+        }
+        if (event.type == Event::LostFocus) {
+            focus = false;
+        }
+        if (event.type == Event::GainedFocus) {
+            focus = true;
+        }
+
+        if (event.type == Event::Resized) {
+            windowWidth = event.size.width;
+            windowHeight = event.size.height;
+        }
+    }
+
+    dt = clock.restart();
+    totalTime += dt;
+
+    frame++;
+}
+
+void Game::draw()
+{
+    window->clear();
+
+    window->display();
+}
+
+bool Game::isWindowOpen()
+{
+    return window->isOpen();
+}
+
+void Game::loadAudio(std::vector<std::string> audioFileNames)
+{
+    for (int i = 0; i < audioFileNames.size(); i++)
+    {
+        sfx.push_back(new Audio());
+        sfx.back()->init(audioFileNames[i]);
+    }
+}
+
+void Game::loadTextures(std::vector<std::string> textureFileNames)
+{
+    for (int i = 0; i < textureFileNames.size(); i++)
+    {
+        Texture texture;
+        std::string path = "textures/" + textureFileNames.at(i);
+        if (!texture.loadFromFile(path))
+            window->close();
+        textures.push_back(texture);
+        std::cout << "Loaded " << path << "\n";
+    }
+}
+
+int Game::randint(int low, int high)
+{
+    int value = rand() % (high + 1 - low) + low;
+    srand(totalTime.asMicroseconds() * value * rand());
+
+    return value;
+}
