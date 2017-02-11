bac048f671bbfe66d9d92124fec2c01c44cf38fc
Pieter Staal <pieterstaal@outlook.com>
2017-02-11 13:52:23 +0100

    Add drawing machines

--- src/Battery.cpp
+++ src/Battery.cpp
@@ -4,10 +4,9 @@
 
 using namespace sf;
 
-Battery::Battery(Vector2i coords, Line* _line)
+Battery::Battery(Vector2i coords)
 {
     this->coords = coords;
-    line = _line;
 }
 
 void Battery::update(Time dt)
@@ -15,14 +14,14 @@ void Battery::update(Time dt)
     charge += line->getPower() * dt.asSeconds();
     charge -= outPower * dt.asSeconds();
 
-    
+
 }
 
 void Battery::draw(RenderWindow* window)
 {
     RectangleShape switchShape;
     switchShape.setSize(Vector2f(40, 40));
-    switchShape.setPosition(coords.x * 40, coords.y * 40);
+    switchShape.setPosition(20 + coords.x * 40, 20 + coords.y * 40);
     switchShape.setFillColor(Color(255, 255, 0));
     window->draw(switchShape);
 }
