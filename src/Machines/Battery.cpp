14a18e65a2a2ee26c7a70846d2ad0684a9ed41ca
WeersProductions <weersproductions@gmail.com>
2017-02-12 15:34:00 +0100

    Tabs to spaces

--- src/Machines/Battery.cpp
+++ src/Machines/Battery.cpp
@@ -1,4 +1,4 @@
-#include "Line.h"
+`#include "Line.h"
 #include <SFML/Graphics.hpp>
 #include "Machines/Battery.h"
 #include "Game.h"
@@ -7,49 +7,49 @@ using namespace sf;
 
 Battery::Battery(Vector2i coords)
 {
-	this->coords = coords;
+    this->coords = coords;
 }
 
 void Battery::update(Time dt)
 {
-	if (broken)
-	{
-		outPower = 0;
-		power = 0;
-	}
-	else
-	{
-		outPower = (-DIFFERENCE/100.0) * (100 - charge / maxCharge * 100) + START_CHARGE;
-	}
+    if (broken)
+    {
+        outPower = 0;
+        power = 0;
+    }
+    else
+    {
+        outPower = (-DIFFERENCE/100.0) * (100 - charge / maxCharge * 100) + START_CHARGE;
+    }
 
-	charge += line->getPower() * dt.asSeconds();
-	if (charge > 0)
-	{
-		charge -= outPower * dt.asSeconds();
-	}
-	if (charge < 0)
-	{
-		charge = 0;
-	}
+    charge += line->getPower() * dt.asSeconds();
+    if (charge > 0)
+    {
+        charge -= outPower * dt.asSeconds();
+    }
+    if (charge < 0)
+    {
+        charge = 0;
+    }
 
-	charge = charge > maxCharge ? maxCharge : charge;
+    charge = charge > maxCharge ? maxCharge : charge;
 }
 
 void Battery::draw(RenderWindow* window, std::vector<Texture>* textures)
 {
-	if (selected) {
-		drawSelected(window, textures);
-	}
+    if (selected) {
+        drawSelected(window, textures);
+    }
 
-	RectangleShape switchShape;
-	switchShape.setSize(Vector2f(18, charge / maxCharge * 32));
-	switchShape.setScale(Vector2f(1, -1));
-	switchShape.setPosition(31 + coords.x * 40, 54 + coords.y * 40);
-	unsigned int blue = charge / maxCharge * 255;
-	switchShape.setFillColor(Color(255 - blue, blue, 0));
-	window->draw(switchShape);
+    RectangleShape switchShape;
+    switchShape.setSize(Vector2f(18, charge / maxCharge * 32));
+    switchShape.setScale(Vector2f(1, -1));
+    switchShape.setPosition(31 + coords.x * 40, 54 + coords.y * 40);
+    unsigned int blue = charge / maxCharge * 255;
+    switchShape.setFillColor(Color(255 - blue, blue, 0));
+    window->draw(switchShape);
 
-	drawTinyMachine(window, textures, 10);
+    drawTinyMachine(window, textures, 10);
 
     if (broken)
         drawBroken(window, textures);
@@ -57,40 +57,40 @@ void Battery::draw(RenderWindow* window, std::vector<Texture>* textures)
 
 void Battery::drawSelected(RenderWindow* window, std::vector<Texture>* textures)
 {
-	drawType(window, textures, getMachineType());
+    drawType(window, textures, getMachineType());
 
-	if (Game::gameInstance->hasActiveComputer())
+    if (Game::gameInstance->hasActiveComputer())
     {
-		std::string chargeInfo = "CHARGE: " + floatToString(charge / maxCharge * 100, 1) + " PERCENT";
-	    drawString(window, chargeInfo, Vector2f(502, 233), &textures->at(0), Color(0, 200, 0), 100);
+        std::string chargeInfo = "CHARGE: " + floatToString(charge / maxCharge * 100, 1) + " PERCENT";
+        drawString(window, chargeInfo, Vector2f(502, 233), &textures->at(0), Color(0, 200, 0), 100);
 
-		std::string outInfo = "OUTPUT: " + floatToString(outPower, 1) + "W";
-	    drawString(window, outInfo, Vector2f(502, 244), &textures->at(0), Color(0, 200, 0), 100);
-	}
+        std::string outInfo = "OUTPUT: " + floatToString(outPower, 1) + "W";
+        drawString(window, outInfo, Vector2f(502, 244), &textures->at(0), Color(0, 200, 0), 100);
+    }
 
-	drawLargeMachine(window, textures, )
+    drawLargeMachine(window, textures, )
 
 }
 
 float Battery::getPowerForLine(Line* line)
 {
-	if (line == outputLine && charge > 0) {
-		if (charge - outPower <= 0)
-		{
-			return 0;
-		}
-		else
-		{
-			return outPower;
-		}
-	}
-	else
-	{
-		return 0;
-	}
+    if (line == outputLine && charge > 0) {
+        if (charge - outPower <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return outPower;
+        }
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 MachineType Battery::getMachineType()
 {
-	return BATTERY;
+    return BATTERY;
 }
